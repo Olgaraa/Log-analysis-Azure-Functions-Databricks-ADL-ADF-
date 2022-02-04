508505d34e8258fa8474f39184f8495a2a29df93
Olga <80418083+Olgaraa@users.noreply.github.com>
2022-02-04 12:02:54 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-import datetime
 import logging
 import azure.functions as func
 from bs4 import BeautifulSoup
@@ -10,6 +9,7 @@ import os
 import re
 from urllib.request import urlopen
 from azure.storage.filedatalake import DataLakeServiceClient
+from azure.storage.blob import ContainerClient
 
 def write_file_to_adls(datalake_service_client, filesystem_name, file_path, file_data):
     file_client = datalake_service_client.get_file_client(filesystem_name, file_path)
@@ -24,6 +24,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     adls_conn_string = os.environ["connect_str"]
     adls_service_client = DataLakeServiceClient.from_connection_string(conn_str = adls_conn_string)
 
+    container = ContainerClient.from_connection_string(conn_str=adls_conn_string, container_name="logs")
+    blob_list = container.list_blobs()
+
+
     try:
         html_page = requests.get('https://www.secrepo.com/self.logs/')
         html_page.raise_for_status()
@@ -31,32 +35,47 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except Exception as e:
         print("An error has occured", e)
 
+    #creating a list containing all the file names that are currently on the website
     soup = BeautifulSoup(html_page.text, 'html.parser')
-    soup=str(soup)
+    soup=str(soup)  
+    match=re.findall("(access.log.[\d-]+.gz).*(\d{4}-\d{2}-\d{2})",soup)
+    match=list(sum(match, ()))
+    file_name=[]
+    for i in range(0, len(match)//2 - 1): file_name.append(match[2*i]+"_"+match[2*i+1])
 
-    #matching the files that were modified today    
-    date_now=datetime.datetime.now().date()
-    pattern = re.compile("<a.href.*log.*</a>."+str(date_now)+".*K")
-    match=re.findall(pattern,soup)
-    match=str(match)
-    print(match)
-    pattern2=re.compile("[a-z]*access.log.....-..-...gz")
-    match2=re.findall(pattern2,match)
-    print(match2)
-    #removing duplicates
-    match2=list(dict.fromkeys(match2))
-    #removing the ".gz"
-    file_name=[i.strip('.gz') for i in match2]
-    #downloading and unzipping the files
+    number_of_elements = len(file_name)
+    print("Total number of access logs on the website:", number_of_elements)
+
+    #creating a list containing all the file names that are currently on ADLS
+    adls_files=[]
+    for x in blob_list:
+        adls_files.append(x)
+    adls_file_names = [ value['name'] for value in adls_files]
+
+    number_of_elements2 = len(adls_file_names)
+    print("Total number of access logs on ADLS:", number_of_elements2)
+   
+    #checking the difference between the list with the file_names from the website and the list with the file_names from ADLS
+    difference=[x for x in file_name if x not in adls_file_names]
+
+    number_of_elements3 = len(difference)
+    print("Number of access logs that will be downloaded and uploaded (difference):", number_of_elements3)
+
+    #taking the name of the files without the date, in order to create a url
+    str_diff=str(difference)
+    links=re.findall("access.log.....-..-...gz",str_diff)
+
+    #downloading and unzipping the files that have not been download yet
     filename_position=0
-    for url in match2:
+    for url in links:
         with gzip.open(urlopen("https://www.secrepo.com/self.logs/"+url), 'rb') as f_in:
-            with open(file_name[filename_position], 'wb'):
+            with open(difference[filename_position], 'wb'):
                 data=f_in.read()
+                #uploading the files to ADLS
                 write_file_to_adls(
                 datalake_service_client = adls_service_client,
                 filesystem_name = "logs",
-                file_path = file_name[filename_position],
+                file_path = difference[filename_position],
                 file_data = data)
                 filename_position=filename_position+1
 
